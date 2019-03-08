b76687cfc369bd03a7b875af5a4ea05addae4778
Will Read <will.read@stitchfix.com>
2019-03-07 16:40:23 -0800

    Adds a note about needing all of XCode, because you do

--- scripts/opt-in/qt.sh
+++ scripts/opt-in/qt.sh
@@ -1,6 +1,8 @@
 # From the Capybara-webkit instructions
 # https://github.com/thoughtbot/capybara-webkit/wiki/Installing-Qt-and-compiling-capybara-webkit#macos-high-sierra-1013-macos-sierra-1012-el-capitan-1011-and-yosemite-1010
 
+# NOTE: You will need all of XCode, not just the command line tools :(
+
 pushd $( brew --prefix )/Homebrew/Library/Taps/homebrew/homebrew-core
 git fetch --unshallow
 git checkout 9ba3d6ef8891e5c15dbdc9333f857b13711d4e97 Formula/qt@5.5.rb
